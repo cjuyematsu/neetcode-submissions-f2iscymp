583351b31759350613909d2e91d3aa976e7d997f
Cole Uyematsu <157172416+cjuyematsu@users.noreply.github.com>
2026-04-28 22:06:50 -0700

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        window = defaultdict(int)
+        max_freq = 0
+        j = 0
+
+        for i in range(len(s)):
+            window[s[i]] += 1
+            max_freq = max(max_freq, window[s[i]])
+
+            if i - j + 1 - max_freq > k:
+                window[s[j]] -= 1
+                j += 1
+        
+        return len(s) - j
